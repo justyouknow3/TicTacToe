2f12a43003d4b4483f6ffea07db8828193caaffb
ELDev <elychristian.bucasas@evsu.edu.ph>
2026-02-04 03:22:50 +0800

    this is my latest update yet so far

--- reactproject/src/App.js
+++ reactproject/src/App.js
@@ -4,8 +4,13 @@ function App() {
   return (
     <div className="App">
       <header>
-        
-        
+          <h1>HI! Ely Christian Bucasas</h1>
+          <nav>
+            <a href="" id='home_btn'>Home</a>
+            <a href="" id='about_btn'>About</a>
+            <a href="" id='portfolio_btn'>Portfolio</a>
+            <a href="" id='question_btn'>Questions</a>
+          </nav>
       </header>
     </div>
   );
